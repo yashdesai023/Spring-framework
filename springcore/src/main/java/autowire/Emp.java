870eebdfbb5d7e2f132e06d732d65d87a09d75e0
yashdesai023 <131481202+yashdesai023@users.noreply.github.com>
2025-03-08 13:56:55 +0530

    Autowiring

---
+++ springcore/src/main/java/autowire/Emp.java
@@ -0,0 +1,28 @@
+package autowire;
+
+public class Emp {
+    private Address address;
+
+    public Address getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Emp() {
+    }
+
+    public Emp(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " address='" + getAddress() + "'" +
+            "}";
+    }
+
+}
